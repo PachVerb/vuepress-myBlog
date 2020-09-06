c8913f5df01d2aa0816e267164be236396394d5e
PachVerb <1506262681@qq.com>
2020-09-06 16:34:28 +0800

    docs: 静态资源路径配置

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'icon', href: '/logo.png' }]
+    ['link', { rel: 'icon', href: $withBase('/index.jpg') }]
   ],
   theme: '@vuepress/theme-blog', 
   themeConfig: {
--- src/README.md
+++ src/README.md
@@ -1,7 +1,7 @@
 ---
 title: home
 ---
-![An image]($withBase(/index.jpg))
+![An image]($withBase('/index.jpg'))
 
 # 韩非子
 > 儒以文乱法， 侠以武犯禁
